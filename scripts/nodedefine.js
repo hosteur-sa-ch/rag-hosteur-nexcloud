c811ee5ee19c2abd455bd706dbfb5d8b9d4d5fdf
Florent Gentric <florent@hosteur.com>
2021-09-30 18:37:10 +0200

    Changing node provisionning

---
+++ scripts/nodedefine.js
@@ -0,0 +1,58 @@
+var resp = {
+    result: 0,
+    ssl: !!jelastic.billing.account.GetQuotas('environment.jelasticssl.enabled').array[0].value,
+    nodes: [{
+      nodeType: "apache2",
+      flexibleCloudlets: 16,
+      fixedCloudlets: 1,
+      diskLimit: 30,
+      engine: "php7.4",
+      nodeGroup: "cp",
+      displayName: "Apps"
+    },
+    {
+        nodeType: "postgres13",
+        flexibleCloudlets: 24,
+        fixedCloudlets: 12,
+        diskLimit: 50,
+        nodeGroup: "sqldb",
+        displayName: "Database"
+    },
+    {
+        nodeType: "redis",
+        flexibleCloudlets: 8,
+        fixedCloudlets: 1,
+        diskLimit: 10,
+        nodeGroup: "nosqldb",
+        displayName: "Sessions"
+    }]
+  }
+
+if (${settings.onlyoff:true}) {
+    resp.nodes.push({
+        image: "onlyoffice/documentserver",
+        tag: ${globals.DOCSRV_RV},
+        count: 1,
+        flexibleCloudlets: 16,
+        fixedCloudlets: 1,
+        diskLimit: 10,
+        nodeGroup: "onlyoffice",
+        displayName: "Document Services"
+    })
+}
+
+if (${settings.onlyoff:true}) {
+    resp.nodes.push({
+        image: "elasticsearch",
+        tag: ${globals.ELS_RV},
+        count: 1,
+        flexibleCloudlets: 16,
+        fixedCloudlets: 1,
+        diskLimit: 50,
+        nodeGroup: "elastic",
+        displayName: "Search Service"
+    })
+}
+
+
+return resp;
